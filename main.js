91fb089b3b3565693d7e8699f3a031365e85db89
TonyDMorris <anthonymorris86@googlemail.com>
2019-02-06 19:46:21 +0000

    save now injects html into results via cards

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const imgName = document.getElementById("imgname");
-const imgUrl = document.getElementById("imgurl");
-
-function addImg(){
+function addImg() {
     event.preventDefault()
-  let oldImages = JSON.parse(localStorage.getItem('itemsArray')) || [];
 
-  let newImage = {
-      "name":document.getElementById("imgname").value,
-      "url":document.getElementById("imgurl").value
-  };
-  oldImages.push(newImage)
-  localStorage.setItem('itemsArray', JSON.stringify(oldImages))
+    const imgName = document.getElementById("imgname");
+    const imgUrl = document.getElementById("imgurl");
+    let oldImages = JSON.parse(localStorage.getItem('imagesArray')) || [];
 
+    let newImage = {
+        "name": imgName.value,
+        "url": imgUrl.value
+    };
+    oldImages.push(newImage)
+    localStorage.setItem('imagesArray', JSON.stringify(oldImages))
+
+    updateImages();
+
+}
+
+function updateImages(){
+    let innerHtml = "";
+    let imageBank = JSON.parse(localStorage.getItem('imagesArray')) || [];
+    for(let i = 0; i < imageBank.length; i++){
+        document.getElementById("results").innerHTML += `<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${imageBank[i].url}" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">${imageBank[i].name}</p>
+        </div>
+      </div>`
+     }
+
+    
+    
 }
