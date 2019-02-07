8d4605f274100207a5c685c04acd452f927a29ac
TonyDMorris <40209459+TonyDMorris@users.noreply.github.com>
2019-02-07 09:58:29 +0000

    Update main.js

--- main.js
+++ main.js
@@ -19,13 +19,18 @@ function addImg() {
 function updateImages(){
     document.getElementById("results").innerHTML = null;
     let imageBank = JSON.parse(localStorage.getItem('imagesArray')) || [];
+   
+    
     for(let i = 0; i < imageBank.length; i++){
+       
+        
+        
         document.getElementById("results").innerHTML += `<a href="${imageBank[i].url}"><div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${imageBank[i].url}" alt="Card image cap">
+        <img class="card-img-top" src="${imageBank[i].url}" alt="Card image cap"></a>
         <div class="card-body">
           <p class="card-text">${imageBank[i].name}</p>
         </div>
-      </div></a>`
+      </div>`
      }
 
     
