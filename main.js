29e3aa0e2713e4cb5bbedab53d520ac29a44aa12
TonyDMorris <40209459+TonyDMorris@users.noreply.github.com>
2019-02-08 08:56:08 +0000

    change to updateImages
    possible solution for creating rows of 3 down the page

--- main.js
+++ main.js
@@ -20,19 +20,32 @@ function updateImages(){
     document.getElementById("results").innerHTML = null;
     let imageBank = JSON.parse(localStorage.getItem('imagesArray')) || [];
    
+    document.getElementById("results").innerHTML += `<div class="card-group">`
     
     for(let i = 0; i < imageBank.length; i++){
        
         
-        
+       if(i % 3 > 0){
         document.getElementById("results").innerHTML += `<a href="${imageBank[i].url}"><div class="card" style="width: 18rem;">
         <img class="card-img-top" src="${imageBank[i].url}" alt="Card image cap"></a>
         <div class="card-body">
           <p class="card-text">${imageBank[i].name}</p>
         </div>
       </div>`
-     }
-
+       }
+        else{
+        document.getElementById("results").innerHTML += `</div><div class="card-group"><a href="${imageBank[i].url}"><div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${imageBank[i].url}" alt="Card image cap"></a>
+        <div class="card-body">
+          <p class="card-text">${imageBank[i].name}</p>
+        </div>
+      </div>`
+        
+        }
+       }
+    
+    
+document.getElementById("results").innerHTML += `</div>`
     
     
 }
